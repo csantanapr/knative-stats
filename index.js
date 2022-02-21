c37bc0b498a6db7092b4a2a35973dbd595bc5b83
Carlos Santana <csantana23@gmail.com>
2022-02-21 15:07:48 -0500

    update to use tags for releases
    Signed-off-by: Carlos Santana <csantana23@gmail.com>

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import parse from 'parse-link-header'
 import { Octokit } from 'octokit'
 // Create a personal access token at https://github.com/settings/tokens/new?scopes=repo
-if( ! process.env.GITHUB_TOKEN ){
+if (!process.env.GITHUB_TOKEN) {
   process.exit()
 }
 const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN })
@@ -28,20 +28,15 @@ async function githubstats (org) {
   })
   await Promise.all(data.data.map(async (repo) => {
     stats.stars = stats.stars + repo.stargazers_count
-    const releases = parseInt(await githubreleases(org, repo.name))
-    stats.releases = stats.releases + releases
     const pulls = parseInt(await githubpulls(org, repo.name))
     stats.pulls = stats.pulls + pulls
     const issues = parseInt(await githubissues(org, repo.name)) - pulls
     stats.issues = stats.issues + issues
     const contributors = await githubcontributors(org, repo.name)
-    // console.log(contributors)
     stats.contributors = union(contributors, stats.contributors)
-
     stats.repos.push({
       name: repo.name,
       stars: repo.stargazers_count,
-      releases: releases,
       issues: issues,
       pulls: pulls,
       contributors: contributors.size
@@ -50,17 +45,13 @@ async function githubstats (org) {
   return stats
 }
 
-async function githubreleases (org, repo) {
-  const data = await octokit.rest.repos.listTags({
+async function githubtags (org, repo) {
+  const data = await octokit.paginate(octokit.rest.repos.listTags, {
     owner: org,
-    repo: repo,
-    per_page: 1
+    repo: repo
   })
-  // console.log(data)
-  const link = parse(data.headers.link)
-  // console.log(link)
-  const count = link && link.last && link.last.page ? link.last.page : 0
-  return count
+  const tags = data.map((tag) => tag.name).filter(tag => tag.startsWith('v'))
+  return tags
 }
 
 async function githubissues (org, repo) {
@@ -93,36 +84,13 @@ async function githubpulls (org, repo) {
 }
 
 async function githubcontributors (org, repo) {
-  const contributors = new Set()
-
-  const data = await octokit.rest.repos.listContributors({
+  const data = await octokit.paginate(octokit.rest.repos.listContributors, {
     owner: org,
     repo: repo,
-    anon: true,
-    per_page: 1
+    anon: true
   })
-  // console.log(data)
-  const link = parse(data.headers.link)
-  const count = link && link.last && link.last.page ? link.last.page : 0
-  // console.log("counts = ",count)
-  const loops = Math.ceil(count / 100)
-  // console.log("loops = ",loops)
-  for (let i = 1; i < loops + 1; i++) {
-    // console.log("doing loop i ",i)
-    const data = await octokit.rest.repos.listContributors({
-      owner: org,
-      repo: repo,
-      anon: true,
-      per_page: 100,
-      page: i
-    })
-    await Promise.all(data.data.map(async (user) => {
-      const id = user.login ? user.login : user.email
-      contributors.add(id)
-    }))
-  }
-  // console.log(contributors)
-  // console.log(contributors.size)
+  const users = data.map((user) => user.login ? user.login : user.email)
+  const contributors = new Set(users)
   return contributors
 }
 
@@ -130,34 +98,36 @@ async function main () {
   const stats = {}
   stats.knative = await githubstats('knative')
   stats['knative-sandbox'] = await githubstats('knative-sandbox')
-  await githubcontributors('knative', 'serving')
+  stats.releases = await githubtags('knative', 'serving')
   return stats
+}
+
+function printNumber (n) {
+  return n.toLocaleString('en-US')
 }
 
 async function printReport (stats) {
   console.log('----Knative---')
-  console.log(`${stats.knative.stars} \tGithub Stars`)
-  console.log(`${stats.knative.pulls} \tGitub Pull Requests`)
-  console.log(`${stats.knative.issues} \tGitub Pull Issues`)
-  console.log(`${stats.knative.contributors.size} \tGitub Contributors`)
-  console.log(`${stats.knative.releases} \t Releases`)
+  console.log(`${printNumber(stats.knative.stars)} \tGithub Stars`)
+  console.log(`${printNumber(stats.knative.pulls)} \tGitub Pull Requests`)
+  console.log(`${printNumber(stats.knative.issues)} \tGitub Issues`)
+  console.log(`${printNumber(stats.knative.contributors.size)} \tGitub Contributors`)
 
   console.log('----Knative-Sandbox---')
-  console.log(`${stats['knative-sandbox'].stars} \tGithub Stars`)
-  console.log(`${stats['knative-sandbox'].pulls} \tGitub Pull Requests`)
-  console.log(`${stats['knative-sandbox'].issues} \tGitub Pull Issues`)
-  console.log(`${stats['knative-sandbox'].contributors.size} \tGitub Contributors`)
-  console.log(`${stats['knative-sandbox'].releases} \tReleases`)
+  console.log(`${printNumber(stats['knative-sandbox'].stars)} \tGithub Stars`)
+  console.log(`${printNumber(stats['knative-sandbox'].pulls)} \tGitub Pull Requests`)
+  console.log(`${printNumber(stats['knative-sandbox'].issues)} \tGitub Issues`)
+  console.log(`${printNumber(stats['knative-sandbox'].contributors.size)} \tGitub Contributors`)
 
   console.log('----Knative orgs---')
-  console.log(`${stats.knative.stars + stats['knative-sandbox'].stars} \tGithub Stars`)
-  console.log(`${stats.knative.pulls + stats['knative-sandbox'].pulls} \tGitub Pull Requests`)
-  console.log(`${stats.knative.issues + stats['knative-sandbox'].issues} \tGitub Pull Issues`)
-  console.log(`${union(stats.knative.contributors, stats['knative-sandbox'].contributors).size} \tGitub Contributors`)
-  console.log(`${stats.knative.releases + stats['knative-sandbox'].releases} \tReleases`)
+  console.log(`${printNumber(stats.knative.stars + stats['knative-sandbox'].stars)} \tGithub Stars`)
+  console.log(`${printNumber(stats.knative.pulls + stats['knative-sandbox'].pulls)} \tGitub Pull Requests`)
+  console.log(`${printNumber(stats.knative.issues + stats['knative-sandbox'].issues)} \tGitub Issues`)
+  console.log(`${printNumber(union(stats.knative.contributors, stats['knative-sandbox'].contributors).size)} \tGitub Contributors`)
+  console.log(`${printNumber(stats.releases.length)} \tReleases`)
 }
 
 main().then(stats => {
-  //console.log(JSON.stringify(stats, null, 4))
+  // console.log(JSON.stringify(stats, null, 4))
   printReport(stats)
 })
